ecc2fc106f324b8a4d448ce74b9cc559da2841b4
ech58 <ech58@duke.edu>
2021-07-01 17:13:56 -0400

    trying to format to string

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -8,7 +8,7 @@ def my_template():
     ts = time.gmtime()
     data = request.get_json
     f = open("log.txt", "a")
-    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + data + "\n")
+    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(data) + "\n")
     f.close()
     return render_template("my_template.html")
 
