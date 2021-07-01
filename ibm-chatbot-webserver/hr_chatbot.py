586c49d77fd8b3a51f2a6cb6dd7c216bc90ccddb
ech58 <ech58@duke.edu>
2021-07-01 17:47:17 -0400

    trying

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -4,18 +4,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def get_request_data():
-    return (
-        request.args
-        or request.form
-        or request.get_json(force=True, silent=True)
-        or request.data
-    )
 def my_template():
     ts = time.gmtime()
-    data = request.get_data()
+    data = request.args
     f = open("log.txt", "a")
-    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(get_request_data) + "\n")
+    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(data) + "\n")
     f.close()
     return render_template("my_template.html")
 
