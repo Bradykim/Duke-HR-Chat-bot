fed20164e63b7dcc15f44d4409672ad1a53e4388
ech58 <ech58@duke.edu>
2021-07-01 17:43:00 -0400

    trying smth else

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -4,11 +4,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def get_request_data():
+    return (
+        request.args
+        or request.form
+        or request.get_json(force=True, silent=True)
+        or request.data
+    )
 def my_template():
     ts = time.gmtime()
     data = request.get_data()
     f = open("log.txt", "a")
-    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(data) + "\n")
+    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(get_request_data) + "\n")
     f.close()
     return render_template("my_template.html")
 
