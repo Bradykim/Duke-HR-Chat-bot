cf07e80b1b82ca522c7a536401bae99a64af06ed
ech58 <ech58@duke.edu>
2021-07-01 16:15:41 -0400

    added time

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request, g
 import time
-
 app = Flask(__name__)
 
-@app.before_request
-def before_request():
-    return str(time.time())
 
 @app.route("/")
 def my_template():
 	return render_template("my_template.html")
 
 f = open("log.txt", "w")
-f.write(str(before_request()) + "   ")
+f.write(time.ctime(time.time) + "   ")
 f.close()
