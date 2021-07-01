5223c076902598d120fbb807c1733530fa3fa3a9
ech58 <ech58@duke.edu>
2021-07-01 15:57:47 -0400

    switched order

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -3,13 +3,13 @@ import time
 
 app = Flask(__name__)
 
-@app.route("/")
-def my_template():
-	return render_template("my_template.html")
-
 @app.before_request
 def before_request():
     return str(time.time())
+
+@app.route("/")
+def my_template():
+	return render_template("my_template.html")
 
 f = open("log.txt", "w")
 f.write(str(before_request()) + "   ")
