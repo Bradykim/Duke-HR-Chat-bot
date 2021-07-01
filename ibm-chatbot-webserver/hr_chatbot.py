bad09192b1ec259580c184ff9f1656cae924f405
ech58 <ech58@duke.edu>
2021-07-01 16:37:28 -0400

    moving order around

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request, g
 import time
 app = Flask(__name__)
 
+ts = time.gmtime()
+
+f = open("log.txt", "a")
+f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + request + "\n")
+f.close()
 
 @app.route("/")
 def my_template():
 	return render_template("my_template.html")
 
-ts = time.gmtime()
 
-f = open("log.txt", "a")
-f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "   " + request + "\n")
-f.close()
