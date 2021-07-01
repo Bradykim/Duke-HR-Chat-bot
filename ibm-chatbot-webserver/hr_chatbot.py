6fd6c6fd2787fdf64fa4b0795ae13d751b843fe5
ech58 <ech58@duke.edu>
2021-07-01 18:52:06 +0000

    added timestamp to log

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def my_template():
 	return render_template("my_template.html")
+
+@app.before_request
+def start_timer():
+    g.start = time.time()
+
+f = open("log.txt", "w")
+f.write(start_timer() + "   ")
+f.close()
