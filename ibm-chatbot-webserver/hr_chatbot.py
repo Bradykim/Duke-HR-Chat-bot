20dd9b7ef44053da50c49f0a374aecae46ad17c1
ech58 <ech58@duke.edu>
2021-07-01 17:52:49 -0400

    adding things

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -4,11 +4,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def foo():
+    data = request.json
+    return jsonify(data)
 def my_template():
     ts = time.gmtime()
     data = request.args
     f = open("log.txt", "a")
-    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(data) + "\n")
+    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(foo) + "\n")
     f.close()
     return render_template("my_template.html")
 
