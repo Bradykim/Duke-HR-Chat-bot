7ffd031549560623832be92dfa7c483aba300040
ech58 <ech58@duke.edu>
2021-07-02 09:54:34 -0400

    still trying

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 import time
+import json
 app = Flask(__name__)
 
 
 @app.route("/")
 def my_template():
     ts = time.gmtime()
-    data = request.form
+    data = request.json
+    str_data = json.dumps(data)
     f = open("log.txt", "a")
-    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str(data) + "\n")
+    f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str_data + "\n")
     f.close()
     return render_template("my_template.html")
