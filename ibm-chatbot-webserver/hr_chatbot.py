2d767848a28b87c4d3baf30e5c38148ec10bdb18
ech58 <ech58@duke.edu>
2021-07-08 10:40:17 -0400

    trying this

--- ibm-chatbot-webserver/hr_chatbot.py
+++ ibm-chatbot-webserver/hr_chatbot.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def my_template():
     ts = time.gmtime()
     data = request.json
-    str_data = json.dumps(data)
+    str_data = json.dumps(request)
     f = open("log.txt", "a")
     f.write(time.strftime("%Y-%m-%d %H:%M:%S", ts) + "\t" + str_data + "\n")
     f.close()
